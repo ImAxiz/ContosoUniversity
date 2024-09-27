9276e59e81ba14a702831970463201981f779873
ImAxiz <markovassiljev01@gmail.com>
2024-09-27 09:59:35 +0300

    .

--- ContosoUniverstity/Controllers/DepartmentsController.cs
+++ ContosoUniverstity/Controllers/DepartmentsController.cs
@@ -1,5 +1,9 @@
 ﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Controllers
@@ -12,10 +16,50 @@ namespace ContosoUniversity.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> index()
+
+        public async Task<IActionResult> Index()
         {
             var schoolContext = _context.Departments.Include(d => d.Administrator);
+            return View(await schoolContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            string query = "SELECT * FROM Department WHERE DepartmentId = {0}";
+            var department = await _context.Departments
+                .FromSqlRaw(query, id)
+                .Include(d => d.Administrator)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "Id", "FullName");
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Budget,StartDate,RowVersion,InstructorId,Aadress,Status")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(department);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "Id", "FullName", department.InstructorID);
+            return View(department);
         }
     }
 }
