69dc6cba2d0ac9ba2cd5038e6c141d903c8b4861
marcusdotnet <124517919+marcusdotnet@users.noreply.github.com>
2024-09-30 16:40:05 +0300

    fixed not able to open Departments

--- ContosoUniverstity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniverstity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd937744c16e9997063aaed14fcb7b0b2252fc27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9276e59e81ba14a702831970463201981f779873")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
