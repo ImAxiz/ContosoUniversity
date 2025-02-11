ed873be99a39744d899a2d75242e434d37aef855
Jamarare <markovassiljev01@gmail.com>
2025-02-11 16:21:17 +0200

    test

--- ContosoUniverstity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniverstity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63b8033c7228cb6a9317fec0c7272a63c3cda4fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2feeb1ee2d3d8a0985c92e803a420c040a24d572")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
